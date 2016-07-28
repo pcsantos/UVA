1be9246aa45c085d8b7e5dc163a7588aed866d13
Paulo Santos <pcsantos05@gmail.com>
2016-07-27 21:29:19 -0300

    Problem10018

---
+++ Problem10018/Main.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+class Main {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfTestCases = Integer.parseInt(input.readLine());
+        for (int i = 0; i < numberOfTestCases; i++) {
+            String number = input.readLine();
+            int times = 0;
+            do {
+                number = reverseAndAdd(number);
+                times++;
+            } while (isNotPalindrome(number));
+            System.out.println(times + " " + number);
+        }
+    }
+
+    private static String reverseAndAdd(String number) {
+        String reverse = new StringBuffer(number).reverse().toString();
+        long addend1 = Integer.parseInt(number);
+        long addend2 = Integer.parseInt(reverse);
+
+        return "" + (addend1 + addend2);
+    }
+
+    private static boolean isNotPalindrome(String number) {
+    
+        String reverse = new StringBuffer(number).reverse().toString();
+        if (number.equals(reverse))
+            return false;
+        return true;
+    }
+}
