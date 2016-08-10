e7b1402e409b0dd32eec844d63eb4fd733088fd8
Paulo Santos <pcsantos05@gmail.com>
2016-08-10 17:24:43 -0300

    Problem 10533 solving Time Limit

--- CodesJava/Problem10533/Gerador.java
+++ CodesJava/Problem10533/Gerador.java
@@ -1,9 +1,9 @@
 class Gerador {
 
     public static void main(String[] args) {
-        System.out.println(5000);
-        for (int i = 0; i < 5000; i++)
-        System.out.println(Math.round(Math.random()*10) + " " + Math.round(Math.random()*1000000));
+        System.out.println(500);
+        for (int i = 0; i < 500; i++)
+         System.out.println(Math.round(Math.random()*10 + 1) + " " + Math.round(Math.random()*999999));
     }
 
 }
--- CodesJava/Problem10533/Main.java
+++ CodesJava/Problem10533/Main.java
@@ -5,45 +5,37 @@ class Main {
 
     private static int HIGH = 1000000;
     private static boolean[] primes = new boolean[HIGH + 1];
+    private static int[] primeDigits = new int[HIGH + 1];
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder stringBuilder = new StringBuilder(25000);
         int testCases = Integer.parseInt(input.readLine());
+        StringTokenizer st;
         String in;
         sieveOfErastosthenes();
+        countDigits();
         long start = System.currentTimeMillis();
         for (int i = 0; i < testCases; i++) {
             in = input.readLine();
-            StringTokenizer st = new StringTokenizer(in);
+            st = new StringTokenizer(in);
             int low = Integer.parseInt(st.nextToken());
             int high = Integer.parseInt(st.nextToken());
-            int out = countNumberOfPrimeDigits(low, high);
+            int out = primeDigits[high] - primeDigits[low];
             stringBuilder.append(out+"\n");
+            if (stringBuilder.length() > 24900) {
+                output.write(stringBuilder.toString());
+                stringBuilder.delete(0, stringBuilder.length());
+            }
         }
         output.write(stringBuilder.toString());
-        output.write("Total: " + (System.currentTimeMillis() - start)/1000);
         output.flush();
         output.close();
         input.close();
     }
 
-    private static int countNumberOfPrimeDigits(int low, int high) {
-    
-        int count = 0;
-        for (int i = low; i <= high; i++) {
-            if (primes[i]) {
-               if (primes[sumDigits(i)])
-                   count++;
-            }
-        }
-        return count;
-    }
-
-
     private static int sumDigits(int number) {
-    
         String n = String.valueOf(number);
         int sum = 0;
         for (int i = 0; i < n.length(); i++) {
@@ -51,16 +43,26 @@ class Main {
         }
         return sum;
     }
-
+    
+    private static void countDigits() {
+        int count = 0;
+        primeDigits[0] = 0;
+        for (int i = 1; i <= HIGH; i++) {
+            if (primes[i]) {
+                if (primes[sumDigits(i)]) {
+                    count++;
+                }
+            }
+            primeDigits[i] = count;
+        }
+    }
     private static void sieveOfErastosthenes() {
 
-        primes[2] = true;
-        for (int i = 3; i <= HIGH; i++) {
-            if (i % 2 != 0)
-                primes[i] = true;
+        for (int i = 2; i <= HIGH; i++) {
+            primes[i] = true;
         }
 
-        for (long i = 3; i <= HIGH; i += 2) {
+        for (long i = 2; i <= Math.sqrt(HIGH); i++) {
             if (primes[(int)i]) {
                 for (long j = i; i*j <= HIGH; j++) {
                     primes[(int)(i*j)] = false;
